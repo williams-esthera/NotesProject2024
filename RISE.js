504d620a513f5b8052cefbd2b7f80db15270a688
aprempeh-tech <prempehangel4@gmail.com>
2024-12-05 16:35:00 -0500

    asd

--- RISE.js
+++ RISE.js
@@ -3,6 +3,10 @@ const  loginLink= document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
+const email = document.getElementById('email')
+const pass = document.getElementById('pass')
+const btn = document.querySelector('.btn')
+
 
 
 registerLink.addEventListener('click', ()=> {
@@ -20,3 +24,33 @@ btnPopup.addEventListener('click', ()=> {
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
 });
+
+function saveData(){
+   
+    const e = document.getElementsByClassName('Register');
+    localStorage.setItem('userData', e)
+    userName = localStorage.getItem('userData')
+}
+
+function login(){;
+    fetch("login.txt")
+    .then(Response => Response.text())
+     .then(data => {
+        var users = data.split('\n');
+        var valid  = false;
+        for (var i = 0; i < users.length; i++){
+            var user = users[i].split(',');
+             if (user[0] === email && user[1] === pass){
+                valid = true;
+                break;
+             }
+        }
+     })
+      if (valid === true){
+        alert ('F**king finaly')
+      } else {
+        alert (' WHYYYYYYY!!!!!!!')
+      }
+    }
+
+    btn.addEventListener('click',login)
